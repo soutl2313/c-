d7cd5067007f1be2d7f32efb5ead1eb6098cdd8a
soutl2313 <prozaxar310@gmail.com>
2025-11-28 12:36:57 +0300

    Add files via upload

---
+++ all (2).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float x;
+    cout <<"введите значение x" <<endl;
+    cin >> x
+    y=sqrt(pow(x,4))+sqrt(abs(x+1))
+    cout <<"y="<<set precission(4)<<y<<endl;
+    retutn 0;
+	// your code goes here
+
+}
